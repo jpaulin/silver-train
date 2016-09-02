aad8234b04d132f98f0a019aa593b202c2833e3b
Jukka Paulin <jukka.paulin@gmail.com>
2016-09-02 13:14:49 +0300

    Added -v flag for showing version

--- chk-users.sh
+++ chk-users.sh
@@ -1,31 +1,40 @@
 #!/bin/bash
 
-# pi-safe : A suite of tools to check your standalone Raspberry Pi
-#           on the configuration side (ie. passwd file; etc.)
+# silver-train : A suite of tools to check your standalone Raspberry Pi
+#                on the configuration side (ie. passwd file; etc.)
 #
 # Check that the user's shell is safe, ie. only intended
 # users have an actual login shell in /etc/passwd
 
-# EXIT-CODE COMPLIANT
-# This script honors the exit code proposal:
+# This script tries its best to honor the 'exit code proposal'
 #  http://tldp.org/LDP/abs/html/exitcodes.html
-# 
-#  In TL;DR it says "Use codes 64..113, and 0 for success"
+#  (=> TL;DR it says "Use codes 64..113, and 0 for success")
 #
 # My exit codes
 # ---------------------------------------
-# Exit code	Meaning
+# Exitcode  Meaning
 #
-# 0	OK - /etc/passwd is safe
-# 64    I/O_ERROR        not able to open/read file /etc/passwd
-# 65	NOT_COVERED      Some entries are missing the field for shell
-# 66	UNSAFE           the /etc/passwd users have known logon shells
-# 67    UNSAFE_UNKNOWN - /etc/passwd users have unknown logon shells
+# 0	    OK - /etc/passwd is safe
+# 64        I/O_ERROR        not able to open/read file /etc/passwd
+# 65	    NOT_COVERED      Some entries are missing the field for shell
+# 66	    UNSAFE           the /etc/passwd users have known logon shells
+# 67        UNSAFE_UNKNOWN - /etc/passwd users have unknown logon shells
+# 68        VERSIONECHO      User supplied 'v' or '--version' request
 
+myname="silver-train"
+myver="0.0.1"
+myorgdate="02-Sep-2016"
 sysname="Raspberry Pi's"
 userfile="/etc/passwd"
+
+# If quietmode is 1, then the script will only communicate via
+# exit codes. Nice for standalone action; perpetual checks (if
+# this script is part of a cron task or similar situations)
 quietmode=0
 
+# Debug mode is a verbose mode that spits out the internal variable
+# values. 
+debugmode=0
 
 function checkPwdFile() {
     # We want only 1 user to be able to log on interactively
@@ -34,7 +43,9 @@ function checkPwdFile() {
     # Unknown shell programs will be listed as violating the safety
     # Ie. exit code
     shellist=`cat $userfile | cut -d : -f 7 | sort | uniq`
-    echo $shellist
+    if [ $quietmode -gt 0 ]; then
+	echo $shellist
+    fi
 }
 
 
@@ -43,6 +54,10 @@ while [[ $# -gt 0 ]]; do
     key="$1"
     # Single option arguments - no secondary parameters
     case $key in
+	-v|--version)
+	    echo "$myname $myver"
+	    exit 68
+	    ;;
 	-h|--help)
 	    echo "You are helped."
 	    exit 0
@@ -58,6 +73,7 @@ while [[ $# -gt 0 ]]; do
     shift
 done
 
+if [
 echo Flags now
 echo "quietmode: $quietmode"
 echo "------------"
@@ -67,6 +83,7 @@ if [ ! -f $userfile ]; then
     exit 64
 fi
 
+# Get unique shells, and check for security requirements
 echo "Following logon shells found on system:"
 checkPwdFile
 
